cef587b680d1c0963618812cc046f1e4e3a656c9
adam malec <add.malec@gmail.com>
2022-08-31 12:19:28 +0300

    add routes

--- worker.js
+++ worker.js
@@ -1,12 +1,38 @@
 import http from 'http';
+import fs from 'fs';
+
+const HOST = '127.0.0.1';
+const PORT = 3000;
 const pid = process.pid;
+
+const routeMap = {
+  '': 'pages/index.html',
+  'works': 'pages/works.html',
+  'about': 'pages/about.html',
+}
 
 export const worker = () => {
   return http
     .createServer((request, response) => {
-      for (let i = 0; i < 1e7; i++) { }
-      response.end('Hello NodeJS!');
-    }).listen(3000, '127.0.0.1', function () {
+      const url = request.url;
+      render(response, routeMap[url.slice(1)]);
+
+    }).listen(PORT, HOST, function () {
       console.log(`Worker started. PID: ${pid}`);
     });
 };
+
+function render(res, htmlFile) {
+  fs.stat(`./${htmlFile}`, (err, stats) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    if (stats) {
+      fs.createReadStream(htmlFile).pipe(res);
+    } else {
+      res.statusCode = 404;
+      res.end('Sorry, page not found!');
+    }
+  });
+}
+
+worker();
