860348c14aa9fd352eb7ebe0a179cc5679f8992f
adam malec <add.malec@gmail.com>
2022-09-01 13:08:46 +0300

    add Nunjucks templating engine

--- worker.js
+++ worker.js
@@ -1,42 +1,37 @@
 import http from 'http';
 import { URL } from 'url';
-import fs from 'fs';
+import nunjucks from 'nunjucks';
 
 const HOSTNAME = '127.0.0.1';
 const PORT = 3000;
-// HOST = `${HOSTNAME}:${PORT}`                    *-memento-*
 const pid = process.pid;
 
+nunjucks.configure('views', { autoescape: true });
+
 const routeMap = {
-  '': 'pages/index.html',
-  'works': 'pages/works.html',
-  'about': 'pages/about.html',
+  '': nunjucks.render('index.njk'),
+  'works': nunjucks.render('works.njk'),
+  'about': nunjucks.render('about.njk'),
 }
 
 export const worker = () => {
   return http
-    .createServer((request, response) => {
-      const requestURL = new URL(request.url, `http://${HOSTNAME}/`);
+    .createServer((req, res) => {
+      const requestURL = new URL(req.url, `http://${HOSTNAME}/`);
       // standardize the requested url by removing any '/' at the start or end
       // '/folder/to/file/' becomes 'folder/to/file'
       let path = requestURL.pathname.replace(/^\/+|\/+$/g, '');
-      render(response, routeMap[path]);
+      if (routeMap[path]) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(routeMap[path]);
+      } else {
+        res.statusCode = 404;
+        res.end('Sorry, page not found!');
+      }
     }).listen(PORT, HOSTNAME, function () {
       console.log(`Worker started. PID: ${pid}`);
     });
 };
-
-function render(res, htmlFile) {
-  fs.stat(`./${htmlFile}`, (err, stats) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    if (stats) {
-      fs.createReadStream(htmlFile).pipe(res);
-    } else {
-      res.statusCode = 404;
-      res.end('Sorry, page not found!');
-    }
-  });
-}
 
 worker();
